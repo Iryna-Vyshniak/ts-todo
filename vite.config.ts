dc5f5f7b4a925420c6657becade630dd8e0f69de
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-10-25 00:06:58 +0300

    test deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     // Reference: https://vitejs.dev/guide/static-deploy.html#github-pages
+    build: {
+        rollupOptions: {
+            input: "dist/app.js",
+        },
+    },
     base: "/ts-todo/",
 });
